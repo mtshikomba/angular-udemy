856840f73db12741339eb2e7bfe4b4ea9b366077
Tangeni <tangenishikomba@gmail.com>
2018-02-24 02:20:42 +0200

    register method in the controller created

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service'
+import { error } from 'util';
 
 @Component({
   selector: 'app-register',
@@ -7,13 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
   }
 
   onSubmit(form){
-    console.log(form.value);
+    console.log(form.value.email);
+    this.authService.register(form.value.name, form.value.email, form.value.password).then((data) => {
+      console.log(data)
+    }, (error) => {
+      console.log(error)
+    })
   }
 
 }
