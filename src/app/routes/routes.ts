316cae2937d4d591b2c1bfdec2d1b59a68137149
Tangeni <tangenishikomba@gmail.com>
2018-03-01 21:20:14 +0200

    guards added to the routes

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,10 +1,11 @@
 import { RegisterComponent } from "../register/register.component";
 import { DashboardComponent} from "../dashboard/dashboard.component";
 import { LoginComponent } from "../login/login.component";
-import { AuthGuard } from "../guards/auth.guard"
+import { AuthGuard } from "../guards/auth/auth.guard";
+import { AuthedGuard } from "../guards/authed/authed.guard"
 
 export const ROUTES = [
-    { path: 'auth/register', component: RegisterComponent},
+    { path: 'auth/register', component: RegisterComponent, canActivate: [AuthedGuard]},
     { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
-    { path: 'auth/login', component: LoginComponent}
+    { path: 'auth/login', component: LoginComponent, canActivate: [AuthedGuard]}
 ];
