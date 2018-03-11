ce500f67502e7abbc37530a72e2a67a2b0de33d0
Tangeni <tangenishikomba@gmail.com>
2018-03-11 16:45:34 +0200

    joke service added

---
+++ src/app/services/joke.service.ts
@@ -0,0 +1,35 @@
+import { toPromise } from "rxjs/operator/toPromise"
+import { Injectable } from '@angular/core';
+import {AuthService} from "./auth.service";
+import {Http, Headers, RequestOptions} from "@angular/http";
+import {CONFIG} from "../config/config";
+import {User} from "../classes/user";
+
+@Injectable()
+export class JokeService {
+
+    private headers: Headers;
+
+    constructor(
+        private authService: AuthService,
+        private http: Http,
+    ) {
+
+        this.headers = new Headers({'Authorization': `Bearer ${this.authService.getToken()}`});
+
+    }
+
+  createJoke(joke: any): Promise<any>{
+      let url = `${CONFIG.API_URL}/jokes`;
+      let body = { title: joke.title, joke: joke.content };
+      let option = new RequestOptions({ headers: this.headers});
+
+
+      return this.http.post(url, body, option )
+          .toPromise()
+          .then(response => {
+              return response.json().data;
+          });
+  }
+
+}
