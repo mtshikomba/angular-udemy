25b4d853f4eab617408f842db1603046d79e590b
Tangeni <tangenishikomba@gmail.com>
2018-03-11 13:40:22 +0200

    create-joke form added to routes

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -6,6 +6,7 @@ import { AuthedGuard } from "../guards/authed/authed.guard"
 import { ProfileComponent} from "../profile/profile.component"
 import { WallComponent} from "../profile/wall/wall.component"
 import { EditProfileComponent} from "../profile/edit-profile/edit-profile.component"
+import { CreateJokeComponent } from "../create-joke/create-joke.component"
 
 export const ROUTES = [
     { path: 'auth/register', component: RegisterComponent, canActivate: [AuthedGuard]},
@@ -13,6 +14,7 @@ export const ROUTES = [
     { path: 'auth/login', component: LoginComponent, canActivate: [AuthedGuard]},
     { path: 'user/profile/:id', component: ProfileComponent, canActivate: [AuthGuard],
         children: [{ path: '', component: WallComponent },
-                   { path: 'edit', component: EditProfileComponent}]}
+                   { path: 'edit', component: EditProfileComponent}]},
+    { path: 'joke/create', component: CreateJokeComponent, CanActivate: [AuthGuard]}
 
 ];
