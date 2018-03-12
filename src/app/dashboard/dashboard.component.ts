bdd466a7f6bbd49f58cd07d47e4fd3de0dbf9748
Tangeni <tangenishikomba@gmail.com>
2018-03-12 20:13:08 +0200

    dashboard completed

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {JokeService} from "../services/joke.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  private data;
+  public isLoading = true;
+
+  constructor(
+      private jokeService: JokeService
+  ) { }
 
   ngOnInit() {
+      this.getJokes();
   }
 
+
+  getNextJokes(){
+    this.getJokes(this.data.next_page_url);
+  }
+
+  getPrevJokes(){
+      this.getJokes(this.data.prev_page_url);
+  }
+
+
+  getJokes(endPoint = null) {
+      this.jokeService.getJokes(endPoint)
+          .then(response => {
+            console.log(response);
+              this.data = response;
+              this.isLoading = false;
+          });
+  }
 }
